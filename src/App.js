550d6ea661016e214843fc83127d29ab8b4e127a
Manoj <manoj09633@gmail.com>
2022-10-23 12:32:09 +0530

    incomplete tasks are shown first using filterHandler method

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   useEffect(() => {
-    console.log("hey");
     filterHandler();
   }, [todos, status]);
   const filterHandler = () => {
@@ -23,26 +22,36 @@ function App() {
         setFilteredTodos(todos.filter((todo) => todo.completed === false));
         break;
       default:
-        setFilteredTodos(todos);
+        setFilteredTodos(() => {
+          for (let i = 0; i < todos.length; i++) {
+            if (todos[i].completed === true) {
+              todos.push(todos[i]);
+              todos.splice(i, 1);
+            }
+          }
+          return todos;
+        });
         break;
     }
   };
 
   return (
     <>
-      <h1 className="todo-title">Simple Todo List</h1>
-      <Form
-        setInputText={setInputText}
-        todos={todos}
-        setTodos={setTodos}
-        inputText={inputText}
-        setStatus={setStatus}
-      />
-      <TodoList
-        todos={todos}
-        setTodos={setTodos}
-        filteredTodos={filteredTodos}
-      />
+      <div className="main-content">
+        <h1 className="todo-title">Simple Todo List</h1>
+        <Form
+          setInputText={setInputText}
+          todos={todos}
+          setTodos={setTodos}
+          inputText={inputText}
+          setStatus={setStatus}
+        />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          filteredTodos={filteredTodos}
+        />
+      </div>
     </>
   );
 }
